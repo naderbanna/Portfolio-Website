bc4b7038d2d7b45c4139a2030d4fa3f31cc0d7fb
naderbanna <naderbanna2@gmail.com>
2021-11-11 18:26:54 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+    return(
+        <Container>
+            <Box borderRadius="lg" bg="lightblue" p={3} mb={6} align="center">
+                Hello, I&apos;m a full-stack developer based in California!
+                </Box>
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                       Nader Banna 
+                    </Heading>
+                    <p>Web Developer.... lalal add some whitty description of myself</p>
+                </Box>
+            </Box>
+        </Container>
+    )
+    
+}
+
+export default Page
